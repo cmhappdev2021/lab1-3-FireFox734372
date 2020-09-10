389cff4012b97d9a3e95e9eee307705fd45d42e6
FireFox734372 <argilbert39@gmail.com>
2020-09-10 18:56:07 +0000

    Finished I think. There may be some problems that I couldn't figure out that I would like to discuss in class

--- main.swift
+++ main.swift
@@ -35,23 +35,44 @@ diameter = 2 * radius
 circumference = 2 * pi * radius. */
 
 let pi = 3.1415927
-
+let radius = 5.0 //woo type inferencing
+let diameter: Double = 2 * radius
+let circumference = diameter * pi
+print(diameter)
+print(circumference)
 
 // Your fitness tracker keeps track of users' heart rate, but you might also want to display their average heart rate over the last hour. Create three constants, heartRate1, heartRate2, and heartRate3. Give each constant a different value between 60 and 100. Create a constant addedHR equal to the sum of all three heart rates. Now create a constant called averageHR that equals addedHR divided by 3 to get the average. Print the result.
 
-
+let heartRate1 = 60
+let heartRate2 = 75
+let heartRate3 = 97
+let addedHR = heartRate1 + heartRate2 + heartRate3
+let averageHR = addedHR / 3
+print (averageHR)
 
 // Now create three more constants, heartRate1D, heartRate2D, and heartRate3D, equal to the same values as heartRate1, heartRate2, and heartRate3. These new constants should be of type Double. Create a constant addedHRD equal to the sum of all three heart rates. Create a constant called averageHRD that equals the addedHRD divided by 3 to get the average of your new heart rate constants. Print the result. Does this differ from your previous average? Why or why not?
 
-
+let heartRate1D: Double = 60
+let heartRate2D: Double = 75
+let heartRate3D: Double = 97
+let addedHRD: Double = heartRate1D + heartRate2D + heartRate3D 
+let averageHRD: Double = addedHRD / 3
+print(averageHRD)
+print("This is different from the precivus result because it was allowed to have a decimal")
 
 // Imagine that partway through the day a user has taken 3,467 steps out of the 10,000 step goal. Create constants steps and goal. Both will need to be of type Double so that you can perform accurate calculations. steps should be assigned the value 3,467, and goal should be assigned 10,000. Create a constant percentOfGoal that equals an expression that evaluates to the percent of the goal that has been achieved so far.
 
-
+var steps: Double = 3_467
+let goal: Double = 10_000
+var percentOfGoal: Double = (steps / goal) * 100
+print(percentOfGoal)
 
 // Declare a variable whose value begins at 10. Using addition, update the value to 15 using the compound assignment operator. Using multiplication, update the value to 30 using compound assignment. Print out the variable's value after each assignment.
 
-
+var ten = 10
+ten += 5
+ten *= 2
+print(ten)
 
 /* Create a variable called piggyBank that begins at 0. You will use this to keep track of money you earn and spend. For each point below, use the right compound assignment operator to update the balance in your piggy bank.
 
@@ -63,56 +84,87 @@ You spend 3 dollars at a convenience store
 
 Print the balance of your piggy bank after each step. */
 
-
+var piggyBank = 0
+piggyBank += 10
+piggyBank += 20
+piggyBank /= 2
+piggyBank *= 3
+piggyBank += 3
+print(piggyBank)
 
 // The most basic feature of your fitness tracking app is counting steps. Create a variable steps and set it equal to 0. Then increment its value by 1 to simulate a user taking a step.
 
-
+var countingSteps = 0
+countingSteps = countingSteps + 1
 
 /* In addition to tracking steps, your fitness tracking app tracks distance traveled. Create a variable distance of type Double and set it equal to 50. This will represent the user having traveled 50 feet.
 
 You decide, however, to display the distance in meters. 1 meter is approximately equal to 3 feet. Use a compound assignment operator to convert distance to meters. Print the result. */
 
-
+var distance: Double = 50
+distance /= 3
+print(distance)
 
 // Print out what you think 10 + 2 * 5 evaluates to. Then print out the actual expression (i.e. print(10 + 2 * 5))
 
-
+print("20")
+print(10 + 2 * 5)
 
 // In a separate print statement, add in the necessary parentheses so that addition takes place before multiplication.
 
-
+print((10 + 2) * 5)
 
 // Print out what you think 4 * 9 - 6 / 2 evaluates to. Then print out the actual expression.
 
-
+print(33)
+print(4 * 9 - 6 / 2)
 
 // In a separate print statement, add in the necessary parentheses so that the subtraction is prioritized over the multiplication and division.
 
-
+print(4 * (9 - 6) / 2)
 
 // If you completed the Fitness Calculations exercise, you calculated an average heart rate to display to the user. However, using proper order of operations you can do this in fewer steps. Create three separate heart rate constants, all of type Double, with values between 60 and 100. Then create a constant equal to the average heart rate. If you use correct order of operations you can do the heart calculation in one line.
 
-
+let heartRateDos1: Double = 67
+let heartRateDos2: Double = 73
+let heartRateDos3: Double = 94
+let heartRateDosAVG: Double = (heartRateDos1 + heartRateDos2 + heartRateDos3) / 3
+print(heartRateDosAVG)
 
 // One feature you might want to give users is to display their current body temperature. Create a constant tempInFahrenheit equal to 98.6.
 
-
+let tempInFahrenheit = 98.6
 
 // You may want to also show the temperature in celsius. You can convert fahrenheit to celsius by taking tempInFahrenheit and subtracting 32, then multiplying the result by (5.0/9.0). Create a constant tempInCelsius that calculates in one line the temperature in celsius.
 
+let tempInCelsius: Double = (tempInFahrenheit - 32) * (5.0 / 9.0)
 
 // Create an integer constant x with a value of 10, and a double constant y with a value of 3.2. Create a constant multipliedAsIntegers equal to x times y. Does this compile? If not, fix it by converting your Double to an Int in the mathematical expression. Print the result.
 
+let x = 10
+/*let y = 3.2
+let multipliedAsIntegers = x * y */
+print("This does compile because you can not multiply an integer and a double")
+let y = 3
+let multipliedAsIntegers = x * y
+print(multipliedAsIntegers)
 
 // Create a constant multipliedAsDoubles equal to x times y, but this time convert the Int to a Double in the expression. Print the result.
 
+let x2: Double = 10.0
+let y2: Double = 3.2
+let multipliedAsDoubles: Double = x2 * y2
+print(multipliedAsDoubles)
 
 // Are the values of multipliedAsIntegers and multipliedAsDoubles different? Print a statement to the console explaining why.
 
+print("The two values are differwent because multipliedAsIntegers did not account for the .2 behind the 3, thus the calculations were different.")
 
 // If you completed the Fitness Calculations exercise, you calculated the percent of the daily step goal that a user has achieved. However, you did this by having steps be of type Double. But you can't really track a partial step, so steps should probably be of type Int. Go ahead and declare steps as type Int and give it a value between 500 and 6,000. Then declare goal as type Int and set it equal to 10,000.
 
-
+let steps2 = 4_372
+let goalSteps = 10_000
 
 // Now create a constant percentOfGoal of type Double that equals the percent of the goal that has been reached so far. You'll need to convert your constants of type Int to be of type Double in your calculation.
+
+let percenGoalSteps = steps2 / goalSteps * 100
